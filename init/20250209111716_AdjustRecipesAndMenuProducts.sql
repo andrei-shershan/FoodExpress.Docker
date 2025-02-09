edc6e630c748c710eaebede36ce9ddb14a355844
Andrei Shershan <mail.shershan@gmail.com>
2025-02-09 13:23:11 +0100

    Adjust DB structure

---
+++ init/20250209111716_AdjustRecipesAndMenuProducts.sql
@@ -0,0 +1,12 @@
+USE food_express_local;
+
+START TRANSACTION;
+
+ALTER TABLE `Recipes` ADD `IsRequired` tinyint(1) NOT NULL DEFAULT FALSE;
+
+ALTER TABLE `MenuPositionProducts` ADD `IsDefault` tinyint(1) NOT NULL DEFAULT FALSE;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20250209111716_AdjustRecipesAndMenuProducts', '8.0.2');
+
+COMMIT;
